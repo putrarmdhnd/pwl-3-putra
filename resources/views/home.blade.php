3634f64701f8ca0ad3370d9ae6b7a0404164ea35
putrarmdhnd <ramadhan.dewantoro95@gmail.com>
2022-11-20 21:43:33 +0700

    js belum jalan

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+<h1 class="m-0 text-dark">Dashboard</h1>
+<div class="col-md-6 my-3">
+    <button class="btn btn-primary">
+        <a href="admin/book" class="text-white">Input buku</a>
+    </button>
+</div>
+@stop
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="col-md-12 text-center">
+                    @if($user->roles_id==1)
+                    <h4>Anda Login sebagai Admin</h4>
+                    @else
+                    <h4>Anda Login sebagai User</h4>
+                    @endif
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@stop
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('admin/home', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.home')->middleware('is_admin');
+
+Route::get('admin/book', [App\Http\Controllers\AdminController::class, 'books'])->name('admin.books')->middleware('is_admin');
+
+Route::post('admin/books', [App\Http\Controllers\AdminController::class, 'submit_book'])->name('admin.book.submit')->middleware('is_admin');
+
+Route::patch('admin/books/update', [App\Http\Controllers\AdminController::class, 'update_book'])->name('admin.book.update')->middleware('is_admin');
+Route::get('admin/ajaxadmin/dataBuku/{id}', [App\Http\Controllers\AdminController::class, 'getDataBuku']);
+
+Route::post('admin/book/delete/{id}', [App\Http\Controllers\AdminController::class, 'delete_book'])->name('admin.book.delete')->middleware('is_admin');
